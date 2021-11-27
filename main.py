09fcd8954b9ab26d3e4f96753bc80e7ffe0b03ee
Nicolas.C <comcomnico@gmail.com>
2021-11-27 14:29:51 +0100

    wip : generate signing key in python

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from Crypto.PublicKey import RSA
 
 def invert_line(img):
     m = img.height // 2             # milieu de l'image
@@ -27,24 +28,34 @@ def bytesToString(bytes):
         msg += chr(nb)
     return msg
 
-
 def hideMessage(img, message):
     mBytes = bytearray(message, "ascii")
-    print(mBytes[0])
     pixels = img.load()
 
+    print(mBytes)
+
+    byteLength = (len(message)*2).to_bytes(16, byteorder='big')
+    print(byteLength)
+    print(byteLength[15])
+
+    print(f"message length : {len(mBytes)}")
     print(f"img width  : {img.width}")
     print(f"img height : {img.height}")
 
-    msgBin = bytesToBinary(mBytes)
-    print(f"msgBin : {msgBin} | length = {len(msgBin)}")
+    for y in range(0, img.height):
+        for x in range(0, img.width):
+            if(x + (x*y) < len(byteLength)):
+                r, g, b = pixels[x,y]
+                g = g ^ byteLength[x + (x*y)]
+                pixels[x, y] = r, g, b
+            else:
+                return
 
     for y in range(0, img.height):
         for x in range(0, img.width):
             if (x + (x*y) < len(mBytes)):
                 r, g, b = pixels[x,y]
                 r = r ^ int(mBytes[x + (x*y)])
-                # r = r ^ int(msgBin[x + (x*y)])
                 pixels[x, y] = r, g, b
             else:
                 return
@@ -60,19 +71,24 @@ def findMessage(img, msg_length):
         for x in range(max_x):
             r, g, b = pixels[x,y]
             mBytes.append(r ^ 255)
+            print(mBytes)
 
     msg = bytesToString(mBytes)
     return msg
+
+def genSigningKey():
+    keyPair = RSA.generate(bits=2048)
+    print("https://cryptobook.nakov.com/digital-signatures/rsa-sign-verify-examples")
 
 def main(filename, output, message):
     # Hide message in picture
     img = Image.open(filename)  # ouverture de l'image contenue dans un fichier
     hideMessage(img, message)
-    img.save(output)            # sauvegarde de l'image obtenue dans un autre fichier
+    # img.save(output)            # sauvegarde de l'image obtenue dans un autre fichier
 
     # Find message in picture
-    img = Image.open(output)
-    print(findMessage(img, len(message)))
+    # img = Image.open(output)
+    # print(findMessage(img, len(message) + 100))
 
 
 if __name__ == "__main__":
