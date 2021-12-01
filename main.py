800517527b1402257a6d85b67ebc223a89983466
FayxChance <fayxchancetom@outlook.com>
2021-12-01 20:21:18 +0100

    Add main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from asyncore import read
+from fileinput import filename
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
@@ -8,6 +10,9 @@ from Crypto.Signature import PKCS1_v1_5
 
 import sys
 import math
+from pprint import pp, pprint
+import io
+
 
 def invert_line(img):
     m = img.height // 2             # milieu de l'image
@@ -15,250 +20,355 @@ def invert_line(img):
 
     for y in range(m, img.height):
         for x in range(0, img.width):
-            r, g, b = pixels[x, y]  # on rÃ©cupÃ¨re les composantes RGB du pixel (x,m)
+            # on rÃ©cupÃ¨re les composantes RGB du pixel (x,m)
+            r, g, b = pixels[x, y]
             r = r ^ 0b11111111      # on les inverse bit Ã  bit avec un XOR
             g = g ^ 0b11111111      # ...
             b = b ^ 0b11111111      # ...
-            pixels[x, y] = r, g, b  # on remet les pixels inversÃ©s dans le tableau
+            # on remet les pixels inversÃ©s dans le tableau
+            pixels[x, y] = r, g, b
 
-def bitfield(n: int):
+
+def bitfield(n: int, size_bytearray: int) -> bytearray:
     """
-    Transform a int into a bit list of size in factor bytes (you know what I mean)
+    bitfield convert an int to a bytearray of size_bytearray
+
+    Args:
+        n (int): int to convert
+        size_bytearray (int): size of the bytearray
+
+    Returns:
+        bytearray: bytearray of size_bytearray
     """
-    bit_list = list()
-    bit_list = [1 if digit=='1' else 0 for digit in bin(n)[2:]]
-    factor = math.ceil(len(bit_list)/8)*8
-    if factor - len(bit_list) != 0:
-        bit_list = [0] * (factor - len(bit_list)) + bit_list
-    
+    bit_list = bytearray()
+    bit_list = [1 if digit == '1' else 0 for digit in bin(n)[2:]]
+    if size_bytearray - len(bit_list) != 0:
+        bit_list = [0] * (size_bytearray - len(bit_list)) + bit_list
+
     return bit_list
 
-def bits_to_int(bit_list: list, delimiter: int) -> list[int]:
+
+def bits_to_int(bit_list: bytearray, delimiter: int) -> list[int]:
     """
-    Transform a bit list into a int
+    bits_to_int convert a bytearray to  delimitered size int list
+
+    Args:
+        bit_list (bytearray): bytearray to convert
+        delimiter (int): size of the delimited int
+
+    Returns:
+        list[int]: int list
     """
-    
     i = 0
     res = list()
     out = 0
-    for bit in bit_list:
-        out = (out << 1) | bit
-        if (i % delimiter) == delimiter - 1:
-            res.append(out)
-            out = 0
-        i += 1
+    bit_list.reverse()
+    bit_list = list(chunks(bit_list, delimiter))
+    for chk in bit_list:
+        for bit in chk:
+            out += bit << i
+            i += 1
+        res += [out]
+        out = 0
+        i = 0
+    res.reverse()
     return res
 
 
-def hide_message(img: Image.Image, m_bytes: bytearray, start_y: int):
-    """
-    Hide in the first line the length of the message encoded in 
-    Hide in the second line the message
-    """
-    byte_list = list()
-    pixels = img.load()
-    
-    
-    byte_list += bitfield(len(m_bytes))
-    
-    for i in range(len(byte_list)//4):
-        x = i  
-        y = start_y
-        print(pixels[x,y])
-        r,_,_ = pixels[x,y]
-        
-        r = 0000 >> r
-        r = r << byte_list[i * 4]
-        r = r << byte_list[i * 4 + 1]    
-        r = r << byte_list[i * 4 + 2]    
-        r = r << byte_list[i * 4 + 3]    
-    
-    byte_list = list()
-    
-    for i in range(len(m_bytes)):
-        byte_list += bitfield(m_bytes[i])
-    
-    for i in range(len(byte_list)//4):
-        x = i  
-        y = start_y + 1 
-        print(pixels[x,y])
-        r,_,_ = pixels[x,y]
-        
-        r = 0000 >> r
-        r = r << byte_list[i * 4]
-        r = r << byte_list[i * 4 + 1]    
-        r = r << byte_list[i * 4 + 2]    
-        r = r << byte_list[i * 4 + 3]    
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
 
 
-def findMessage(img: Image.Image, start_y: int):
-    
-    message = list()
-    
+def hide_message(filename: str, m_bytes: bytearray, start_y: int) -> None:
+    """
+    hide_message in filemane image the message m_bytes in the line start_y
+
+    Args:
+        filename (str): image filename
+        m_bytes (bytearray): message to hide
+        start_y (int): line where the size of the message is hidden, next line is the message itself
+    """
+
+    size_message = len(m_bytes)*2
+    size_message_bin = bitfield(size_message, 16)
+    part_size_message_bin = list(chunks(size_message_bin, 4))
+
+    img = Image.open(filename)
     pixels = img.load()
 
-    m_bytes = []
+    # Write the size of the message in the start_y line
+    for i in range(0, 4):
+        r, g, b = pixels[i, start_y]
+        r = (r >> 4) << 4
+        print(f'part_size_message_bin[i]: {part_size_message_bin[i]}'))
+        r=r ^ bits_to_int(part_size_message_bin[i], 4)[0]
+        pixels[i, start_y]=r, g, b
 
-    # Get message length
-    for i in range(0, 4, 1):
-        x = i
-        y = start_y
-        r,_,_ = pixels[x,y]
-        bit = r & 0b1
-        m_bytes.append(bit)
-        r = r >> 1
-        bit = r & 0b1
-        m_bytes.append(bit)
-        bit = r & 0b1
-        m_bytes.append(bit)
-        r = r >> 1
-        bit = r & 0b1
-        m_bytes.append(bit)
-    
-    size = bits_to_int(m_bytes, 16)
-    
-    m_bytes = []
-    # Get message
-    for i in range(0, size//4):
-        x = i
-        y = start_y + 1
-        r,_,_ = pixels[x,y]
-        bit = r & 0b1
-        m_bytes.append(bit)
-        r = r >> 1
-        bit = r & 0b1
-        m_bytes.append(bit)
-        bit = r & 0b1
-        m_bytes.append(bit)
-        r = r >> 1
-        bit = r & 0b1
-        m_bytes.append(bit)
+    print(len(m_bytes), "bytes")
+    # Write 4 byte by 4 byte the message in the next line
+    for i in range(0, size_message):
+        r, g, b=pixels[i, start_y + 1]
 
-    # Split m_bytes into array of 8 bits
-    for i in range(0, len(m_bytes), 8):
-        message.append(bits_to_int(m_bytes[i:i+8]))
+        r=(r >> 4) << 4
 
+        if i % 2 == 0:
+            r=r ^ ((m_bytes[i//2] >> 4) & 0b00001111)
+        else:
+            r=r ^ (m_bytes[i//2] & 0b00001111)
+        pixels[i, start_y + 1]=r, g, b
+        print("after ", r)
+
+    img.save(filename+"_out.png")
+
+
+def find_message(filename: str, start_y: int) -> bytearray:
+    """
+    Given a line, find the size of the message and the message in the next line
+
+    Args:
+        filename (str): image filename
+        start_y (int): Line where the size of the message is hidden, next line is the message itself
+
+    Returns:
+        bytearray: message
+    """
+    message=list()
+
+    print(f"file: {filename}_out.png")
+    img=Image.open(filename)
+
+    pixels=img.load()
+
+    # Read the size of the message
+    size_message_bin=list()
+    for i in range(0, 4):
+        r, g, b=pixels[i, start_y]
+        size_message_bin += [r & 0b00001111]
+    size=size_message_bin[0] << 12 | size_message_bin[1] << 8 | size_message_bin[2] << 4 | size_message_bin[3]
+    print(f'size: {size}')
+    # Read 4 byte by 4 byte the message in the next line
+    message_part=list()
+    for i in range(0, size):
+        r, g, b=pixels[i, start_y + 1]
+        print(r)
+        message_part += [r & 0b00001111]
+    print(message_part)
+    for i in range(0, size, 2):
+        message += [message_part[i] << 4 | 0b00001111 & message_part[i + 1]]
+
+    print(f"message: {message}")
+
+    print(f'size_message: {size}')
     return message
 
 
 def generate_pair_key():
-    key = RSA.generate(2048)
-    private_key = key.export_key()
-    out = open("private.key", "wb")
+    """
+    generate_pair_key generate a pair of 2048 bit sized keys
+    """
+    key=RSA.generate(2048)
+    private_key=key.export_key()
+    out=open("private.key", "wb")
     out.write(private_key)
     out.close()
 
-    public_key = key.publickey().export_key()
-    out = open("public.key", "wb")
+    public_key=key.publickey().export_key()
+    out=open("public.key", "wb")
     out.write(public_key)
     out.close()
-    
-    
-def sign_data(byte_array: bytearray):
-    
-    f = open(f"certificate/{filename}", "rb")
-    message = f.read()
-    f.close()
 
-    private_key = open("private.key", "r")
-    private_key = RSA.importKey(private_key.read())
-    f.close()
 
-    signer = PKCS1_v1_5.new(private_key)
-    digest = SHA512.new()
-    digest.update(message)
-    sig = signer.sign(digest)
+def sign_data(byte_array: bytearray) -> bytearray:
+    """
+    sign_data sign the byte_array with the private key
 
-    f = open(f'sign/{filename}.sig', "wb")
-    f.write(sig)
-    f.close()
+    Args:
+        byte_array (bytearray): byte_array to sign
+
+    Returns:
+        bytearray: signed byte_array
+    """
+    private_key_file=open("private.key", "r")
+    private_key=RSA.importKey(private_key_file.read())
+    private_key_file.close()
+
+    signer=PKCS1_v1_5.new(private_key)
+    digest=SHA512.new()
+    digest.update(bytes(byte_array))
+    sig=signer.sign(digest)
+
+    return sig
+
 
 def sign_file(filename: str):
-    
-    f = open(f"certificate/{filename}", "rb")
-    message = f.read()
+    """
+    sign_file sign the file with the private key
+
+    Args:
+        filename (str): file to sign
+    """
+
+    f=open(f"certificate/{filename}", "rb")
+    message=f.read()
     f.close()
 
-    private_key = open("private.key", "r")
-    private_key = RSA.importKey(private_key.read())
+    private_key=open("private.key", "r")
+    private_key=RSA.importKey(private_key.read())
     f.close()
 
-    signer = PKCS1_v1_5.new(private_key)
-    digest = SHA512.new()
-    digest.update(message)
-    sig = signer.sign(digest)
+    signer=PKCS1_v1_5.new(private_key)
+    digest=SHA512.new()
+    digest.update(bytes(message))
+    sig=signer.sign(digest)
 
-    f = open(f'sign/{filename}.sig', "wb")
+    f=open(f'sign/{filename}.sig', "wb")
     f.write(sig)
     f.close()
 
-def validate_certificate(filename: str):
-    f = open(f'certificate/{filename}.png', "rb")
-    image_bin = f.read()
+
+def validate_certificate(filename: str) -> bool:
+    """
+    validate_certificate validate the certificate of the file
+
+    Args:
+        filename (str): file to validate
+
+    Returns:
+        bool: True if the certificate is valid, False otherwise
+    """
+    f=open(f'certificate/{filename}', "rb")
+    image_bin=f.read()
     f.close()
-    
-    f = open(f'sign/{filename}.png.sig', "rb")
-    sign = f.read()
+
+    f=open(f'sign/{filename}.sig', "rb")
+    sign=f.read()
     f.close()
-    
+
     return validate_data(image_bin, sign)
 
-def validate_hidden_data(filename: str):
-    img = Image.open(f'{filename}.png')
-    
-    name_sign = findMessage(img, 1)
-    firstname_sign = findMessage(img, 3)
-    
-    cypher_name = findMessage(img, 5)
-    cypher_firstname = findMessage(img, 7)
-    
-    return findMessage(img, 512, 1)
 
-def validate_data(data: bytearray, signature: bytearray):
-    
-    # name_sign = findMessage(img, 512, 1)
-    # firstname_sign = findMessage(img, 512, 2)
-    
-    # cypher_name = findMessage(img, 512, 3)
-    # cypher_firstname = findMessage(img, 512, 4)
+def validate_hidden_data(filename: str) -> bool:
+    """
+    validate_hidden_data validate the hidden data of the file
 
-    hash = SHA512.new()
-    
-    public_key_file = open("public.key", "r")
-    public_key = RSA.importKey(public_key_file.read())
-    
-    verifier = PKCS1_v1_5.new(public_key)
-    
+    Args:
+        filename (str): file to validate
+
+    Returns:
+        bool: True if the hidden data is valid, False otherwise
+    """
+
+    filename=f'certificate/{filename}'
+
+    firstname_plain=find_message(filename, 1)
+    firstname_sign=find_message(filename, 3)
+
+    firstname_valid=validate_data(firstname_plain, firstname_sign)
+    print(f'firstname_valid: {firstname_valid}')
+
+    # name_plain = find_message(filename, 5)
+    # name_sign = find_message(filename, 7)
+
+    # name_valid = validate_data(name_plain, name_sign)
+
+    # score_plain = find_message(filename, 9)
+    # score_sign = find_message(filename, 11)
+
+    # score_valid = validate_data(score_plain, score_sign)
+
+    # university_footprint = find_message(filename, 13)
+    # university_footprint_sign = find_message(filename, 15)
+
+    # university_footprint_valid = validate_data(
+    #     university_footprint, university_footprint_sign)
+
+    # return firstname_valid and name_valid and score_valid and university_footprint_valid
+    return False
+
+
+def validate_data(data: bytearray, signature: bytearray) -> bool:
+    """
+    validate_data validate the data with the public key
+
+    Args:
+        data (bytearray): data to validate
+        signature (bytearray): signature of the data
+
+    Returns:
+        bool: True if the data is valid, False otherwise
+    """
+    hash=SHA512.new()
+
+    public_key_file=open("public.key", "r")
+    public_key=RSA.importKey(public_key_file.read())
+
+    verifier=PKCS1_v1_5.new(public_key)
+
     hash.update(data)
-    
-    return verifier.verify(hash, signature)
-   
-def generate_certificate(name: str, firstname: str, score: int):
-    
-    def text(img, text, x, y, text_size, font="arial", fill=(0,0,0)):
-        draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype(f'fonts/{font}.ttf', text_size)  
-        draw.text((x, y), text, fill, font)  
 
-    img = Image.open(f'diplome-BG.png')
-    
-    text(img, 'UNSC', 350, 150, 80, fill=(0,0,255))
-    
-    text(img, f'Spartan', 380, 270, 50, fill=(0,128,0))
-    
-    text(img, f"{name.upper()} {firstname.upper()} a réussi la formation de l'UNSC", 200, 375, 30, fill=(0,0,0))
-    
-    text(img, f'avec une moyenne de {score}', 325, 425, 30, fill=(0,0,0))
-        
-    hide_message(img, firstname, start_y)
-    
-    img.save(f'certificate/{name.upper()}_{firstname.upper()}.png')
-    sign_certificate(f'{name.upper()}_{firstname.upper()}.png')
-    
-    
+    try:
+        verifier.verify(hash, signature)
+        return True
+    except (ValueError, TypeError):
+        return False
+
+
+def generate_certificate(name: str, firstname: str, score: int):
+    """
+    generate_certificate generate a certificate for the user and save it in the certificate folder. And sign it with the private key in the sign folder.
+
+    Args:
+        name (str): name of the user
+        firstname (str): firstname of the user
+        score (int): score of the user
+    """
+    def text(img, text, x, y, text_size, font = "arial", fill = (0, 0, 0)):
+        draw=ImageDraw.Draw(img)
+        font=ImageFont.truetype(f'fonts/{font}.ttf', text_size)
+        draw.text((x, y), text, fill, font)
+
+    img=Image.open(f'diplome-BG.png')
+
+    text(img, 'UNSC', 350, 150, 80, fill = (0, 0, 255))
+
+    text(img, f'Spartan', 380, 270, 50, fill = (0, 128, 0))
+
+    text(img, f"{name.upper()} {firstname.upper()} a réussi la formation de l'UNSC",
+         200, 375, 30, fill = (0, 0, 0))
+
+    text(img, f'avec une moyenne de {score}', 325, 425, 30, fill = (0, 0, 0))
+
+    footprint="UNSC Institute of Reach"
+
+    filename=f'{name}_{firstname}.png'
+
+    img_path=f'certificate/{filename}'
+
+    img.save(img_path)
+
+    hide_message(img_path, firstname.encode('ascii'), 1)
+    # hide_message(img_path, sign_data(firstname.encode('ascii')), 3)
+    # hide_message(img_path, name.encode('ascii'), 5)
+    # hide_message(img_path, sign_data(name.encode('ascii')), 7)
+    # hide_message(img_path, bitfield(len(bitfield(score, 16)), 16), 9)
+    # hide_message(img_path, sign_data(bitfield(score, 16)), 11)
+    # hide_message(img_path, footprint.encode('ascii'), 13)
+    # hide_message(img_path, sign_data(footprint.encode('ascii')), 15)
+
+    sign_file(filename)
+
+
 def main(filename, output, message):
-    generate_certificate("117", "JOHN", 20)
-    print(validate_certificate("117_JOHN"))
+    # generate_certificate("117", "john", 20)
+    # print(validate_certificate("117_john.png"))
+    # print(validate_hidden_data("117_john.png"))
+    print(bitfield(4, 4))
+    print(bits_to_int([1, 0, 0, 1, 1, 0, 0, 0], 4))
+    hide_message("certificate/117_john.png", "TEST".encode('ascii'), 1)
+    find_message("certificate/117_john.png", 1)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
