d6f7ab2f30d8859fc1c43dd28967eeac3b87b4d9
Nicolas.C <comcomnico@gmail.com>
2021-12-01 21:52:35 +0100

    writing and reading message with length on lsb

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import math
 from pprint import pp, pprint
 import io
 
+TAILLE = 16
 
 def invert_line(img):
     m = img.height // 2             # milieu de l'image
@@ -28,141 +29,85 @@ def invert_line(img):
             # on remet les pixels inversÃ©s dans le tableau
             pixels[x, y] = r, g, b
 
+def get_LSB_of_int(color):
+    return color & 1
 
-def bitfield(n: int, size_bytearray: int) -> bytearray:
-    """
-    bitfield convert an int to a bytearray of size_bytearray
+def stringToBinary(string):
+    return ''.join(format(x, '08b') for x in bytearray(string, 'ascii'))
 
-    Args:
-        n (int): int to convert
-        size_bytearray (int): size of the bytearray
+def binaryToString(binary):
+    # split fullBin en tableau de binaires de longueur 7
+    binTab = [binary[i:i+8] for i in range(0, len(binary), 8)]
 
-    Returns:
-        bytearray: bytearray of size_bytearray
-    """
-    bit_list = bytearray()
-    bit_list = [1 if digit == '1' else 0 for digit in bin(n)[2:]]
-    if size_bytearray - len(bit_list) != 0:
-        bit_list = [0] * (size_bytearray - len(bit_list)) + bit_list
+    # transforme chanque binaire de taille 7 en un charactère
+    msg = ""
+    for b in binTab:
+        msg += chr(int(b, 2))
 
-    return bit_list
+    return msg
 
-
-def bits_to_int(bit_list: bytearray, delimiter: int) -> list[int]:
-    """
-    bits_to_int convert a bytearray to  delimitered size int list
-
-    Args:
-        bit_list (bytearray): bytearray to convert
-        delimiter (int): size of the delimited int
-
-    Returns:
-        list[int]: int list
-    """
-    i = 0
-    res = list()
-    out = 0
-    bit_list.reverse()
-    bit_list = list(chunks(bit_list, delimiter))
-    for chk in bit_list:
-        for bit in chk:
-            out += bit << i
-            i += 1
-        res += [out]
-        out = 0
-        i = 0
-    res.reverse()
-    return res
-
-
-def chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
-
-
-def hide_message(filename: str, m_bytes: bytearray, start_y: int) -> None:
-    """
-    hide_message in filemane image the message m_bytes in the line start_y
-
-    Args:
-        filename (str): image filename
-        m_bytes (bytearray): message to hide
-        start_y (int): line where the size of the message is hidden, next line is the message itself
-    """
-
-    size_message = len(m_bytes)*2
-    size_message_bin = bitfield(size_message, 16)
-    part_size_message_bin = list(chunks(size_message_bin, 4))
-
-    img = Image.open(filename)
+def hide_message(img, message, y_index):
+    # transforme notre message en binaire
+    msgBin = stringToBinary(message)
     pixels = img.load()
 
-    # Write the size of the message in the start_y line
-    for i in range(0, 4):
-        r, g, b = pixels[i, start_y]
-        r = (r >> 4) << 4
-        print(f'part_size_message_bin[i]: {part_size_message_bin[i]}'))
-        r=r ^ bits_to_int(part_size_message_bin[i], 4)[0]
-        pixels[i, start_y]=r, g, b
+    msgLengthBin = format(len(message), '016b')
 
-    print(len(m_bytes), "bytes")
-    # Write 4 byte by 4 byte the message in the next line
-    for i in range(0, size_message):
-        r, g, b=pixels[i, start_y + 1]
+    print("_____ ECRITURE _____")
+    print(f"msgBin  = {msgBin}\nmsgBin length  = {len(msgBin)}")
 
-        r=(r >> 4) << 4
+    # inscrit dans le bit de poids faible du rouge de chaque pixel un bit de notre taille de message
+    for i in range(TAILLE):
+        x = i % img.width
+        y = (i // img.width) % img.height
+        r,_,_ = pixels[x,y]
+        r = (r & ~1) | int(msgLengthBin[x + (x*y)])
+        pixels[x,y] = r,_,_
 
-        if i % 2 == 0:
-            r=r ^ ((m_bytes[i//2] >> 4) & 0b00001111)
-        else:
-            r=r ^ (m_bytes[i//2] & 0b00001111)
-        pixels[i, start_y + 1]=r, g, b
-        print("after ", r)
+    # inscrit dans le bit de poids faible du rouge de chaque pixel un bit de notre fullbin
+    y_index += 1
+    for i in range(len(msgBin)):
+        x = i % img.width
+        y = y_index + (i // img.width) % img.height
+        r,_,_ = pixels[x,y]
+        r = (r & ~1) | int(msgBin[i])
+        pixels[x,y] = r,_,_
 
-    img.save(filename+"_out.png")
+def find_message_length(img, y_index):
 
+    pixels = img.load()
 
-def find_message(filename: str, start_y: int) -> bytearray:
-    """
-    Given a line, find the size of the message and the message in the next line
+    msgLengthBin = ""
 
-    Args:
-        filename (str): image filename
-        start_y (int): Line where the size of the message is hidden, next line is the message itself
+    # récupère le bit de poids faible de la couleur rouge de chaque pixels
+    for i in range(TAILLE):
+        r,_,_ = pixels[i,y_index]
+        msgLengthBin += str(get_LSB_of_int(r))
 
-    Returns:
-        bytearray: message
-    """
-    message=list()
+    print("_____ LECTURE TAILLE  _____")   
+    print(f"msgLengthBin = {msgLengthBin}\nmsgLengthBin length = {len(msgLengthBin)}")
 
-    print(f"file: {filename}_out.png")
-    img=Image.open(filename)
+    print("_____ RESULTAT TAILLE _____")
+    return int(msgLengthBin, 2)
 
-    pixels=img.load()
+def find_message(img, msg_length, y_index):
+    
+    pixels = img.load()
 
-    # Read the size of the message
-    size_message_bin=list()
-    for i in range(0, 4):
-        r, g, b=pixels[i, start_y]
-        size_message_bin += [r & 0b00001111]
-    size=size_message_bin[0] << 12 | size_message_bin[1] << 8 | size_message_bin[2] << 4 | size_message_bin[3]
-    print(f'size: {size}')
-    # Read 4 byte by 4 byte the message in the next line
-    message_part=list()
-    for i in range(0, size):
-        r, g, b=pixels[i, start_y + 1]
-        print(r)
-        message_part += [r & 0b00001111]
-    print(message_part)
-    for i in range(0, size, 2):
-        message += [message_part[i] << 4 | 0b00001111 & message_part[i + 1]]
+    fullBin = ""
 
-    print(f"message: {message}")
+    # récupère le bit de poids faible de la couleur rouge de chaque pixels
+    for i in range(msg_length * 8):
+        x = i % img.width
+        y = y_index + (i // img.width) % img.height
+        r,_,_ = pixels[x,y]
+        fullBin += str(get_LSB_of_int(r))
 
-    print(f'size_message: {size}')
-    return message
+    print("_____ LECTURE MESSAGE  _____")   
+    print(f"fullBin = {fullBin}\nfullBin length = {len(fullBin)}")
 
+    print("_____ RESULTAT MESSAGE _____")
+    return binaryToString(fullBin)
 
 def generate_pair_key():
     """
@@ -361,14 +306,22 @@ def generate_certificate(name: str, firstname: str, score: int):
 
 
 def main(filename, output, message):
+    # Hide message in picture
+    img = Image.open(filename).convert("RGB")  # ouverture de l'image contenue dans un fichier
+    hide_message(img, message, 0)
+    img.save(output)            # sauvegarde de l'image obtenue dans un autre fichier
+
+    # Find message in picture
+    img = Image.open(output)
+    msgLength = find_message_length(img, 0)
+    print(f"msgLength : {msgLength}")
+    print(find_message(img, msgLength, 1))
+
+    # ------------------------------------------------------------------------------------------
+
     # generate_certificate("117", "john", 20)
     # print(validate_certificate("117_john.png"))
     # print(validate_hidden_data("117_john.png"))
-    print(bitfield(4, 4))
-    print(bits_to_int([1, 0, 0, 1, 1, 0, 0, 0], 4))
-    hide_message("certificate/117_john.png", "TEST".encode('ascii'), 1)
-    find_message("certificate/117_john.png", 1)
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
