2ff56a9d3baddff8eb542d783f2eeef969f2c384
FayxChance <fayxchancetom@outlook.com>
2021-12-02 11:19:51 +0100

    Add comment and fix validate

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from asyncore import read
-from fileinput import filename
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
@@ -9,14 +7,18 @@ from Crypto.Hash import SHA512
 from Crypto.Signature import PKCS1_v1_5
 
 import sys
-import math
-from pprint import pp, pprint
-import io
 
+# Size of stored length in bits
 TAILLE = 16
 
 
-def invert_line(img):
+def invert_line(img: Image.Image) -> None:
+    """
+    invert_line invert half of the image
+
+    Args:
+        img (Image.Image): image to invert
+    """
     m = img.height // 2             # milieu de l'image
     pixels = img.load()             # tableau des pixels
 
@@ -31,27 +33,61 @@ def invert_line(img):
             pixels[x, y] = r, g, b
 
 
-def get_LSB_of_int(color):
+def get_LSB_of_int(color: int) -> int:
+    """
+    get_LSB_of_int get low bit of an int
+
+    Args:
+        color (int): int to get the low bit
+
+    Returns:
+        int: lowest bit of the int
+    """
     return color & 1
 
 
-def bytesToBinary(b: bytes):
+def bytes_to_binary(b: bytes) -> bytes:
+    """
+    bytes_to_binary convert a bytes to a binary string
+
+    Args:
+        b (bytes): bytes to convert
+
+    Returns:
+        bytes: binary string converted
+    """
     tmp = b.hex()
     return bin(int(tmp, 16))[2:].zfill(8)
 
 
-def stringToBinary(string):
-    print(f"len {len(string)}, input {string}")
-    res = ''.join(format(x, '08b') for x in bytearray(string, 'utf-8'))
-    print(f"output {res}, len {len(res)}")
-    return res
+def string_to_binary(string: str) -> bytes:
+    """
+    string_to_binary convert a string to a binary string
+
+    Args:
+        string (str): string to convert
+
+    Returns:
+        bytes: binary string converted
+    """
+    return ''.join(format(x, '08b') for x in bytearray(string, 'utf-8'))
 
 
-def binaryToString(binary):
-    # split fullBin en tableau de binaires de longueur 7
+def binary_to_string(binary: bytes) -> str:
+    """
+    binary_to_string convert a binary string to a string
+
+    Args:
+        binary (bytes): binary string to convert
+
+    Returns:
+        str: string converted
+    """
+
+    # Split the binary string into 8-bit chunks
     binTab = [binary[i:i+8] for i in range(0, len(binary), 8)]
 
-    # transforme chanque binaire de taille 7 en un charactère
+    # Convert each 8-bit chunk into a string
     msg = ""
     for b in binTab:
         msg += chr(int(b, 2))
@@ -59,26 +95,47 @@ def binaryToString(binary):
     return msg
 
 
-def hide_bytes(img, b, y_index):
-    # transforme notre message en binaire
-    msgBin = bytesToBinary(b)
+def hide_bytes(img: Image.Image, b: bytes, y_index: int):
+    """
+    hide_bytes hide the bytes in the image at (0,y_index)
+
+    Args:
+        img (Image.Image): image to hide the bytes
+        b (bytes): bytes to hide
+        y_index (int): y index in the image to hide the bytes
+    """
+
+    msgBin = bytes_to_binary(b)
     hide_binary(img, b, msgBin, y_index)
 
 
-def hide_message(img, message, y_index):
-    # transforme notre message en binaire
-    msgBin = stringToBinary(message)
+def hide_message(img: Image.Image, message: str, y_index: int):
+    """
+    hide_message hide the message in the image at (0,y_index)
+
+    Args:
+        img (Image.Image): image to hide the message
+        message (str): message to hide
+        y_index (int): y index in the image to hide the message
+    """
+    msgBin = string_to_binary(message)
     hide_binary(img, message, msgBin, y_index)
 
 
-def hide_binary(img, message, binary, y_index):
+def hide_binary(img: Image.Image, message: str, binary: bytes, y_index: int):
+    """
+    hide_binary hide the binary in the image at (0,y_index)
+
+    Args:
+        img (Image.Image): image to hide the binary
+        message (str): used to know the length of the binary
+        binary (bytes): binary to hide
+        y_index (int): y index in the image to hide the binary
+    """
+
     pixels = img.load()
 
     msgLengthBin = format(len(message), '016b')
-
-    # print("_____ ECRITURE TAILLE _____")
-    # print(
-    #     f"msgLengthBin  = {msgLengthBin}\nmsgLengthBin length  = {len(msgLengthBin)}")
 
     # inscrit dans le bit de poids faible du rouge de chaque pixel un bit de notre taille de message
     for i in range(TAILLE):
@@ -87,10 +144,6 @@ def hide_binary(img, message, binary, y_index):
         r, _, _ = pixels[x, y]
         r = (r & ~1) | int(msgLengthBin[i])
         pixels[x, y] = r, _, _
-
-    # print("_____ ECRITURE MESSAGE _____")
-    # print(f"msgBin  = {binary}\nmsgBin length  = {len(binary)}")
-    # inscrit dans le bit de poids faible du rouge de chaque pixel un bit de notre fullbin
 
     for i in range(len(binary)):
         x = i % img.width
@@ -111,21 +164,26 @@ def find_message_length(img, y_index):
         r, _, _ = pixels[i, y_index]
         msgLengthBin += str(get_LSB_of_int(r))
 
-    # print("_____ LECTURE TAILLE  _____")
-    # print(
-    #     f"msgLengthBin = {msgLengthBin}\nmsgLengthBin length = {len(msgLengthBin)}")
-
-    # print("_____ RESULTAT TAILLE _____")
     return int(msgLengthBin, 2)
 
 
-def find_message(img, msg_length, y_index):
+def find_message(img: Image.Image, msg_length: int, y_index: int) -> str:
+    """
+    find_message find the message of size msg_length at (0,y_index) in the image and return it
 
+    Args:
+        img (Image.Image): image to search
+        msg_length (int): size of the message to find
+        y_index (int): y index of the message to find
+
+    Returns:
+        str: message found
+    """
     pixels = img.load()
 
     fullBin = ""
 
-    # récupère le bit de poids faible de la couleur rouge de chaque pixels
+    # Get the lower bits of the red color of each pixel
     for i in range(msg_length * 8):
         x = i % img.width
         y = y_index + (i // img.width) % img.height
@@ -133,11 +191,7 @@ def find_message(img, msg_length, y_index):
 
         fullBin += str(get_LSB_of_int(r))
 
-    # print("_____ LECTURE MESSAGE  _____")
-    # print(f"fullBin = {fullBin}\nfullBin length = {len(fullBin)}")
-
-    # print("_____ RESULTAT MESSAGE _____")
-    return binaryToString(fullBin)
+    return binary_to_string(fullBin)
 
 
 def generate_pair_key():
@@ -175,7 +229,6 @@ def sign_data(byte_array: bytearray) -> bytearray:
     digest.update(bytes(byte_array))
     sig = signer.sign(digest)
 
-    # "".join(map(chr, sig))
     return sig
 
 
@@ -205,12 +258,13 @@ def sign_file(filename: str):
     f.close()
 
 
-def validate_certificate(filename: str) -> bool:
+def validate_certificate(filename: str, signature: str) -> bool:
     """
     validate_certificate validate the certificate of the file
 
     Args:
         filename (str): file to validate
+        signature (str): signature of the file
 
     Returns:
         bool: True if the certificate is valid, False otherwise
@@ -219,7 +273,7 @@ def validate_certificate(filename: str) -> bool:
     image_bin = f.read()
     f.close()
 
-    f = open(f'sign/{filename}.sig', "rb")
+    f = open(f'sign/{signature}', "rb")
     sign = f.read()
     f.close()
 
@@ -247,33 +301,56 @@ def validate_hidden_data(filename: str) -> bool:
     msgLength = find_message_length(img, 10)
     firstname_sign = find_message(img, msgLength, 11)
 
-    firstname_valid = validate_data(firstname_plain.encode("utf-8"), firstname_sign.encode("utf-8"))
+    firstname_valid = validate_data(firstname_plain.encode(
+        "utf-8"), firstname_sign.encode("utf-8"))
+
+    if firstname_valid:
+        print("firstname valid")
+    else:
+        print("firstname invalid")
 
     msgLength = find_message_length(img, 20)
     name_plain = find_message(img, msgLength, 21)
-    
+
     msgLength = find_message_length(img, 30)
     name_sign = find_message(img, msgLength, 31)
 
-    name_valid = validate_data(name_plain.encode("utf-8"), name_sign.encode("utf-8"))
+    name_valid = validate_data(name_plain.encode(
+        "utf-8"), name_sign.encode("utf-8"))
+
+    if name_valid:
+        print("name valid")
+    else:
+        print("name invalid")
 
     msgLength = find_message_length(img, 40)
     score_plain = find_message(img, msgLength, 41)
-    
+
     msgLength = find_message_length(img, 50)
     score_sign = find_message(img, msgLength, 51)
 
-    score_valid = validate_data(score_plain.encode("utf-8"), score_sign.encode("utf-8"))
+    score_valid = validate_data(score_plain.encode(
+        "utf-8"), score_sign.encode("utf-8"))
+
+    if score_valid:
+        print("score valid")
+    else:
+        print("score invalid")
 
     msgLength = find_message_length(img, 60)
     university_footprint = find_message(img, msgLength, 61)
-    
+
     msgLength = find_message_length(img, 70)
     university_footprint_sign = find_message(img, msgLength, 71)
 
     university_footprint_valid = validate_data(
-    university_footprint.encode("utf-8"), university_footprint_sign.encode("utf-8"))
-    
+        university_footprint.encode("utf-8"), university_footprint_sign.encode("utf-8"))
+
+    if university_footprint_valid:
+        print("university footprint valid")
+    else:
+        print("university footprint invalid")
+
     return firstname_valid and name_valid and score_valid and university_footprint_valid
 
 
@@ -297,11 +374,7 @@ def validate_data(data: bytearray, signature: bytearray) -> bool:
 
     hash.update(data)
 
-    try:
-        verifier.verify(hash, signature)
-        return True
-    except (ValueError, TypeError):
-        return False
+    return verifier.verify(hash, signature)
 
 
 def generate_certificate(name: str, firstname: str, score: int):
@@ -346,44 +419,115 @@ def generate_certificate(name: str, firstname: str, score: int):
 
     img.save(img_path)
 
-    # Fonctions de test d'écriture et de lecture de message
-    # Hide message in picture
-    # img = Image.open('diplome-BG.png').convert("RGB")  # ouverture de l'image contenue dans un fichier
-    # # hide_bytes(img, sign_data(firstname.encode('utf-8')), 10)
-    # hide_message(img, "Didier", 3)
-    # img.save(img_path)            # sauvegarde de l'image obtenue dans un autre fichier
-
-    # # Find message in picture
-    # img = Image.open(img_path)
-    # msgLength = find_message_length(img, 10)
-    # print(f"msgLength : {msgLength}")
-    # print(find_message(img, msgLength, 11))
-
     sign_file(filename)
 
 
-def main(filename, output, message):
-    # Fonctions de test d'écriture et de lecture de message
-    # # Hide message in picture
-    # img = Image.open(filename).convert("RGB")  # ouverture de l'image contenue dans un fichier
-    # hide_message(img, message, 0)
-    # img.save(output)            # sauvegarde de l'image obtenue dans un autre fichier
+def help():
+    return """Usage: python3 main.py [commands] [arguments]
+Commands available :
+--help: print this help
+--generate: generate a certificate for the user and save it in the certificate folder. And sign it with the private key in the sign folder.
+    [arguments]: <name>  <firstname> <score>
+    [example]: python3 main.py --generate "Doe" "John" 20
+    [output]: certificate/John_Doe.png and sign/John_Doe.png
 
-    # # Find message in picture
-    # img = Image.open(output)
-    # msgLength = find_message_length(img, 0)
-    # print(f"msgLength : {msgLength}")
-    # print(find_message(img, msgLength, 1))
+--validate: validate the certificate of the user
+    [arguments]: <filename> of the certificate to validate and the signature of the certificate (<filename>.png.sign). Files are directly in the certificate folder, no needs to add ./certificate.
+    [example]: python3 main.py --validate "John_Doe.png" "John_Doe.png.sign"
+    [output]: True or False
 
-    # ------------------------------------------------------------------------------------------
+--read: read the message in the picture at the position <position> and print it
+    [arguments]: <filename> of the picture and the position of the message
+    [example]: python3 main.py --read "John_Doe.png" 0
+    [output]: John
 
-    generate_certificate("117", "john", 20)
-    print(validate_certificate("117_john.png"))
-    print(validate_hidden_data("117_john.png"))
+--hide-message: hide the message in the picture at the position <position>
+    [arguments]: <filename> of the picture and the position of the message and the message to hide
+    [example]: python3 main.py --hide-message "John_Doe.png" 0 "John"
+    [output]: John_Doe.png
+
+--pair: generate a pair of keys and save them in the folder
+    [arguments]: None
+    [example]: python3 main.py --pair
+    [output]: ./public.key and ./private.key
+
+"""
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("usage: {} image output".format(sys.argv[0]))
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
-    sys.exit(1)
+    # If no args are given, print the help
+    if len(sys.argv) == 1:
+        print(help())
+        exit(1)
+    for arg in sys.argv:
+        if arg == "--help":
+            print(help())
+            exit(0)
+        if arg == "--generate":
+            if len(sys.argv) == 5:
+                name = sys.argv[2]
+                firstname = sys.argv[3]
+                score = int(sys.argv[4])
+                generate_certificate(name, firstname, score)
+                exit(0)
+            else:
+                print("""Error: --generate requires 3 arguments
+    [arguments]: <name>  <firstname> <score>
+    [example]: python3 main.py --generate "Doe" "John" 20
+    [output]: certificate/John_Doe.png and sign/John_Doe.png
+""")
+                exit(1)
+        if arg == "--validate":
+            if len(sys.argv) == 4:
+                filename = sys.argv[2]
+                signature = sys.argv[3]
+                print(validate_certificate(filename, signature))
+                exit(0)
+            else:
+                print("""Error: --validate requires 2 arguments
+    [arguments]: <filename> of the certificate to validate and the signature of the certificate (<filename>.png.sign). Files are directly in the certificate folder, no needs to add ./certificate.
+    [example]: python3 main.py --validate "John_Doe.png" "John_Doe.png.sign"
+    [output]: True or False""")
+                exit(1)
+        if arg == "--read":
+            if len(sys.argv) == 3:
+                filename = sys.argv[2]
+                line = int(sys.argv[3])
+                img = Image.open(filename)
+                size = find_message_length(img, line)
+                print(f"Size message found: {size}")
+                print(find_message(img, size, line + 1))
+                exit(0)
+            else:
+                print("""Error: --read requires 2 arguments
+    [arguments]: <filename> of the picture and the position of the message
+    [example]: python3 main.py --read "John_Doe.png" 0
+    [output]: John""")
+                exit(1)
+        if arg == "--hide-message":
+            if len(sys.argv) == 4:
+                filename = sys.argv[2]
+                line = int(sys.argv[3])
+                message = sys.argv[4]
+                img = Image.open(filename)
+                hide_message(img, message, int(line))
+                print(f"Message {message} hidden in {filename} at line {line}")
+                img.save(filename)
+                exit(0)
+            else:
+                print("""Error: --hide-message requires 3 arguments
+    [arguments]: <filename> of the picture and the position of the message and the message to hide
+    [example]: python3 main.py --hide-message "John_Doe.png" 0 "John"
+    [output]: John_Doe.png""")
+                exit(1)
+        if arg == "--pair":
+            if len(sys.argv) == 3:
+                generate_pair_key()
+                print("Keys generated")
+                exit(0)
+            else:
+                print("""Error: --pair requires 2 arguments
+    [arguments]: None
+    [example]: python3 main.py --pair
+    [output]: ./public.key and ./private.key""")
+                exit(1)
